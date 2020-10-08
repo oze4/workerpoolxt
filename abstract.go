96ecc1f16d66ae5cab328ae618136905e650cf42
Matt Oestreich <matthewpoestreich@gmail.com>
2020-10-08 15:21:37 -0500

    splitting code into individual files

--- abstract.go
+++ abstract.go
@@ -64,21 +64,3 @@ type Events []Event
 
 // Action is a func that takes a *Client and returns a Reaction
 type Action func(*Client) Reaction
-
-// Reaction holds response data
-type Reaction struct {
-	Response interface{}
-	Error    error
-	duration time.Duration
-	name     string
-}
-
-// Duration returns duration
-func (r *Reaction) Duration() time.Duration {
-	return r.duration
-}
-
-// Name returns the job name
-func (r *Reaction) Name() string {
-	return r.name
-}
---
+++ reaction.go
@@ -0,0 +1,23 @@
+package reactor
+
+import (
+	"time"
+)
+
+// Reaction holds response data
+type Reaction struct {
+	Response interface{}
+	Error    error
+	duration time.Duration
+	name     string
+}
+
+// Duration returns duration
+func (r *Reaction) Duration() time.Duration {
+	return r.duration
+}
+
+// Name returns the job name
+func (r *Reaction) Name() string {
+	return r.name
+}

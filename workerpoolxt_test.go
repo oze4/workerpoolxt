831bd68be383c2c05562a0a1c22e801921d52831
Matt Oestreich <matthewpoestreich@gmail.com>
2020-10-09 14:23:36 -0500

    starting to add tests

---
+++ workerpoolxt_test.go
@@ -0,0 +1,39 @@
+package workerpoolxt
+
+import (
+	//"reflect"
+	"testing"
+	"time"
+)
+
+func TestOverflow(t *testing.T) {
+	wp := New(2, time.Duration(time.Second*10))
+	releaseChan := make(chan struct{})
+
+	// Start workers, and have them all wait on a channel before completing.
+	for i := 0; i < 64; i++ {
+		wp.SubmitXT(Job{
+			Name: "test1",
+			Task: func() Response { 
+				<-releaseChan 
+				return Response{}
+			},
+		})
+	}
+
+	// Start a goroutine to free the workers after calling stop.  This way
+	// the dispatcher can exit, then when this goroutine runs, the workerpool
+	// can exit.
+	go func() {
+		<-time.After(time.Millisecond)
+		close(releaseChan)
+	}()
+	wp.Stop()
+
+	// Now that the worker pool has exited, it is safe to inspect its waiting
+	// queue without causing a race.
+	qlen := wp.WorkerPool.WaitingQueueSize()
+	if qlen != 62 {
+		t.Fatal("Expected 62 tasks in waiting queue, have", qlen)
+	}
+}

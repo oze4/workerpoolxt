f09ce1097aa8dcb6275e2ce56bf64f348a4d57a8
Matt Oestreich <matthewpoestreich@gmail.com>
2020-10-09 17:18:25 -0500

    adding tests

---
+++ .github/workflows/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ================================================= #
+#                                                   #
+# Thanks to https://github.com/gammazero/workerpool #
+#                                                   #
+# ================================================= #
+
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.out
+        rm profile.out
+    fi
+done

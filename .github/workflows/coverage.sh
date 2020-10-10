c420a4189b6cc97a7e32e482caa4bea045155ee3
Matt Oestreich <matthewpoestreich@gmail.com>
2020-10-09 19:28:51 -0500

    update

--- .github/workflows/coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo $GOPATH
-go get github.com/mattn/goveralls
-go get -u github.com/rakyll/gotest
-$GOPATH/bin/goveralls -coverprofile=coverage.out -service=github -repotoken=$COVERALLS_TOKEN

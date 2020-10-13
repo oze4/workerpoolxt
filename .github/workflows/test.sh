7a6964085aad5ab6a426e944a81a63671637213d
Matt Oestreich <matthewpoestreich@gmail.com>
2020-10-13 01:17:05 -0500

    codacy: fix quoting issue

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -6,17 +6,17 @@ set -e
 
 profile=profile.out; coverage=coverage.out; special_count=2000;
 
-echo "" >$coverage
+echo "" >"$coverage"
 
 for d in $(go list ./... | grep -v vendor); do
     # Basic, defaut test that generates codecov
-    go test -v -race -coverprofile=$profile -covermode=atomic $d
+    go test -v -race -coverprofile="$profile" -covermode=atomic "$d"
     printf "\n%s\n" "[STARTING] : '^Test.*_Special$' : this may take a while"
     # Includes any test ending in `_Special`
-    go test -race -run ^Test.*_Special$ -count=$special_count $d
+    go test -race -run ^Test.*_Special$ -count="$special_count" "$d"
     # Cleanup
-    if [ -f $profile ]; then
-        cat $profile >>$coverage
-        rm $profile
+    if [ -f "$profile" ]; then
+        cat "$profile" >>"$coverage"
+        rm "$profile"
     fi
 done
